b6c953a767455b20172530713ef00b8f3f23a0ce
Lucas Aguiar <lucas.abreu@redeceler.com.br>
2022-10-14 15:03:43 -0300

    ok

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -5,9 +5,45 @@ console.log("[DEPLOY_PREVIEW] Starting deploy preview script");
 const command = "yarn deploy:staging";
 const output = execSync(command, { encoding: "utf-8" });
 
-const outputLines = output.split('\n')
-const DEPLOY_URL = outputLines[outputLines.length - 1]
+const outputLines = output.split("\n");
+const DEPLOY_URL = outputLines[outputLines.length - 1];
 
 console.log("[DEPLOY_PREVIEW] Finished deploy preview script");
 
 console.log(`[DEPLOY_PREVIEW] Deploy URL: ${DEPLOY_URL}`);
+
+console.log("[GITHUB_COMMENT]: START");
+const { GITHUB_TOKEN, GITHUB_REPOSITORY, GITHUB_PR_NUMBER } = process.env;
+const GH_COMMENT = `
+- Deploy URL: [${DEPLOY_URL}](${DEPLOY_URL})
+`;
+
+const defaultHeaders = {};
+defaultHeaders["authorization"] = `token ${GITHUB_TOKEN}`;
+defaultHeaders["accept"] = "application/vnd.github.v3+json; application/vnd.github.antiope-preview+json";
+defaultHeaders["content-type"] = "application/json";
+
+console.log("GITHUB_REPOSITORY", GITHUB_REPOSITORY);
+console.log("GITHUB_PR_NUMBER", GITHUB_PR_NUMBER);
+
+fetch(
+  `https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${GITHUB_PR_NUMBER}/comments`,
+  {
+    headers: defaultHeaders,
+    method: "POST",
+    body: JSON.stringify({
+      body: GH_COMMENT,
+    }),
+  }
+)
+  .then((response) => {
+    if (response.ok) return response.json();
+    throw new Error(response.statusText);
+  })
+  .catch((err) => {
+    console.log("[COMMENT_ON_GITHUB: ERROR]");
+    throw new Error(err);
+  })
+  .finally(() => {
+    console.log("[COMMENT_ON_GITHUB: END]");
+  });

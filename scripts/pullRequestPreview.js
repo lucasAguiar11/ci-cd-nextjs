3e7e706128c51ed7dbc695e4bf62985682528177
Lucas Aguiar <lucas.abreu@redeceler.com.br>
2022-10-14 14:42:21 -0300

    add url

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -5,6 +5,9 @@ console.log("[DEPLOY_PREVIEW] Starting deploy preview script");
 const command = "yarn deploy:staging";
 const output = execSync(command, { encoding: "utf-8" });
 
+const outputLines = output.split('\n')
+const DEPLOY_URL = outputLines[outputLines.length - 1]
+
 console.log("[DEPLOY_PREVIEW] Finished deploy preview script");
 
-console.log(output);
+console.log(`[DEPLOY_PREVIEW] Deploy URL: ${DEPLOY_URL}`);

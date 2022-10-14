cdecbf0d9842918c5fad2ac19c549f219b3b9b4b
Lucas Aguiar <lucas.abreu@redeceler.com.br>
2022-10-14 14:36:19 -0300

    ok

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+import { execSync } from "child_process";
+
+console.log("[DEPLOY_PREVIEW] Starting deploy preview script");
+
+const command = "yarn deploy:staging";
+const output = execSync(command, { encoding: "utf-8" });
+
+console.log("[DEPLOY_PREVIEW] Finished deploy preview script");
+
+console.log(output);
